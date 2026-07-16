fafe6fecee1bc581f5f944f6216df8137877881f
Thanh Quyên <thanhquyen@Macbook-cua-Thanh-Quyen.local>
2026-07-16 12:40:16 +0700

    hoan thanh bai 2

---
+++ demokieudulieu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    unsigned short a,b;
+    a = 65534;
+    for (int i = 0; i < 5; i++)
+    {
+        b= a + i;
+        printf("cộng %d :\t %d\n",i, b);
+    }
+    
+
+    return 0;
+}
